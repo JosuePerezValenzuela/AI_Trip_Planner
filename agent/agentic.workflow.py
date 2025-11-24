8b0c908ea38792d1da06ffc32f11a52ffcdfe832
Josue Perez <josueperezv2004@gmail.com>
2025-11-24 18:50:50 -0400

    code updated for loading a model

---
+++ agent/agentic.workflow.py
@@ -0,0 +1,33 @@
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+# from tools.weather_info_tool import WeatherLoader
+# from tools.place_search_tool import PlaceSearchTool
+# from tools.expense_calculator_tool import CalculatoTool
+# from tools.currency_conversion_tool import CurrencuConverterTool
+
+class GraphBuilder():
+    def __init__(self):
+        self.tools= [
+            #WeatherInfoTool(),
+            #PlaceSearchTool(),
+            #CalculatorTool(),
+            #CurrencyConverterTool()
+        ]
+
+    def agent_function(self):
+        pass
+
+    def build_graph(self):
+        graph_builder = StateGraph(MessagesState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START, "agent")
+        graph_builder.add_conditional_edges("agent", tools_condition)
+        graph_builder.add_edge("tools", "agent")
+        graph_builder.add_edge("agent", END)
+
+    def __call__(self):
+        pass
